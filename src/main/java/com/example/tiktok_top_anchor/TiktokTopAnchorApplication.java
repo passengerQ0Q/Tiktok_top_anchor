1e65a83debf14529cc6f679e6fc0901028a348fe
Administrator <1152449961@qq.com>
2024-01-23 22:52:04 +0800

    初始化

---
+++ src/main/java/com/example/tiktok_top_anchor/TiktokTopAnchorApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiktok_top_anchor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokTopAnchorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiktokTopAnchorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tiktok_top_anchor/TiktokTopAnchorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiktok_top_anchor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktokTopAnchorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
